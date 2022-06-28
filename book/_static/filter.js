3281d7ce51cf3554f9d7c17804f655ad40f58c3d
Naomi Alterman <naomila@uw.edu>
2022-06-28 11:55:50 -0700

    Fix filter functionality

--- book/_static/filter.js
+++ book/_static/filter.js
@@ -1,16 +1,33 @@
 function showOnly(filter) {
-  const shields = document.getElementsByClassName("level-shield");
-  for (shield of shields) {
+  const all_shields = document.getElementsByClassName("level-shield");
+  
+  const shielded_containers = new Set();
+
+  // Build list of filterable page elements
+  for (let shield of all_shields) {
     const parent = shield.parentElement;
     const grandparent = parent.parentElement;
     let container = parent;
     if (grandparent.tagName === "LI") {
       container = grandparent;
     }
-    if (filter !== undefined && !shield.classList.contains(filter)) {
-      container.style.display = "none";
+    shielded_containers.add(container);
+  }
+
+  // For each filterable element, hide if it doesn't
+  // contain the specified filter
+  for (let container of shielded_containers) {
+    let hide_container = true;
+    let container_shields = container.getElementsByClassName("level-shield");
+    for (let shield of container_shields) {
+      if (shield.classList.contains(filter)) {
+        hide_container = false;
+      }
+    }
+    if (filter === undefined || !hide_container) {
+      container.style.display = "";
     } else {
-      container.style.display = "";        
+      container.style.display = "none";
     }
   }
 }
